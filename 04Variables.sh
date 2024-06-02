e808eda462c607c36d7b020d195bc4904867e27c
SHIVA <pabbalas7@gmail.com>
2024-06-02 16:22:49 +0530

    04

---
+++ 04Variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo "$PERSON1: Hi $PERSON2, how are you"
+echo "$PERSON2: Hi $PERSON1, GD MOR"
+echo "$PERSON1: iam good waht about you"
+echo "$PERSON2: Not Bad"
+echo "$PERSON2: How is your life"
