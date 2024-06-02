a8933abea14b79421586d525e679af0a1dd4b933
SHIVA <pabbalas7@gmail.com>
2024-06-02 16:31:29 +0530

    this

--- 05Var.sh
+++ 05Var.sh
@@ -1,7 +1,7 @@
 #1/bin/bash
 
-echo "please enter your username::"
-echo "please enter your password::"
+echo "please enter your username:"
+echo "please enter your password:"
 
 read -s USERNAME
 read -s PASSWORD
