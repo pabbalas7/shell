25b3d20fd6143c74b8c3b6e35924e984fa2cb143
SHIVA <pabbalas7@gmail.com>
2024-06-02 16:38:40 +0530

    inhu

--- 06Var.sh
+++ 06Var.sh
@@ -3,4 +3,4 @@ echo "please enter your user:"
 echo "please enter your password:"
 read -s USERNAME
 read -s PASSWORD
-echo "Username is : $USERNAME,Password is :$PASSWORD
+echo "Username is : $USERNAME,Password is : $PASSWORD"
