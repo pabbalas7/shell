1332b2863d98869d0db8e5a89833ed2208a03ac8
SHIVA <pabbalas7@gmail.com>
2024-06-04 07:56:20 +0530

    seq

---
+++ SQL.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ID=$(id -u)
+if [ $ID -ne 0 ]
+then 
+ echo "Error: Please run with root user"
+ exit 1
+else
+ echo "your a root user"
+ 
+yum install mysql -y
+if [ $? -ne o ]
+then
+ echo "Error:Installing mysql is failed"
+else
+ echo "installing mysql is success"
+fi
+
+yum insatll GIT -y
+if [ $? -ne o ]
+then
+ echo "Error:Installing GIT is failed"
+else
+ echo "installing GIT is success"
+fi
